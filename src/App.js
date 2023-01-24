51c4a1cd50923ece553bf12ecae94e9d94a3819f
Dani Villalba <danivillalba_007@hotmail.com>
2023-01-24 19:07:06 -0300

    asdf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
+      <h1>Hello Dojo</h1>
+      <h2>Things I need to do:</h2>
+
+      <ul>
+        <li>
           Learn React
-        </a>
-      </header>
+        </li>
+        <li>
+          Climb Mt. Everest
+        </li>
+        <li>
+          Run a marathon
+        </li>
+        <li>
+          Feed the dogs
+        </li>
+      </ul>
+
+
     </div>
   );
 }
